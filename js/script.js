6f58c8f3620232472250deeca110d5d702092c99
Noah Walker <nwalker7727@gmail.com>
2024-06-06 15:46:28 -0700

    added api

--- js/script.js
+++ js/script.js
@@ -30,63 +30,54 @@ function prevSlide() {
     }
 }
 
+const URL = "https://perenual.com/api/pest-disease-list";
+
+$("#disease").click(function(){
+  console.log("Click");
+  // call ajax
+  $.ajax(ajaxObj);
+})
+
+const ajaxObj = {
+  url: URL,
+  data: {
+    key: "sk-uCEA6659018900ac35690",
+  },
+  type: "GET",
+  dataType: "json",
+  success: ajaxSuccess,
+  error: ajaxError
+}
+
+function ajaxSuccess(data) {
+  const dataArray = data.data;
+  console.log(dataArray.length)
+  const randomIndex = Math.floor(Math.random() * dataArray.length);
+  console.log(randomIndex)
+  const element1 = dataArray[randomIndex];
+  console.log("element1:", element1);
+  const question1 = element1.description[0].subtitle;
+  const answer1 = element1.description[0].description;
+  const question2 = element1.description[1].subtitle;
+  const answer2 = element1.description[1].description;
+  let imageURL = null;
+  try{
+    imageURL = element1.images[0].regular_url;
+  }
+  catch(error){
+    $.ajax(ajaxObj);
+  }
+  $("#diseased").html("<h3>" + question1);
+  if(imageURL != null){
+     $("#diseased").append("<img src='" + imageURL + "' />")
+  }
+  $("#diseased").append("<p>" + answer1);
+  $("#diseased").append("<h4>" + question2);
+  $("#diseased").append("<p>" + answer2);
+}
 
 
-// function showSlide(index) {
-//     slides.forEach((slide, i) => {
-//         slide.style.transform = `translateX(${100 * (i - index)}%)`;
-//         console.log(index);
-//     });
-// }
-
-// function nextSlide() {
-//     if (slideIndex < slides.length+2) {
-//         slideIndex+=2;
-//         showSlide(slideIndex);
-//     }
-//     else if(slideIndex >= slides.length+2) {
-//         slideIndex = 0;
-//         showSlide(slideIndex);
-//     }
-// }
-
-// function prevSlide() {
-//     if (slideIndex > 0) {
-//         slideIndex-=2;
-//         showSlide(slideIndex);
-//     }
-//     else if(slideIndex <=0) {
-//         slideIndex = slides.length+2;
-//         showSlide(slideIndex);
-//     }
-// }
-
-// let currentSlide = 0;
-
-// function showSlide(index) {
-//     const slides = document.querySelectorAll('.slide');
-//     if (index >= slides.length) {
-//         currentSlide = 0;
-//     } else if (index < 0) {
-//         currentSlide = slides.length - 1;
-//     } else {
-//         currentSlide = index;
-//     }
-
-//     slides.forEach((slide, i) => {
-//         slide.style.transform = `translateX(${(i - currentSlide) * 100}%)`;
-//     });
-// }
-
-// function nextSlide() {
-//     showSlide(currentSlide + 1);
-// }
-
-// function prevSlide() {
-//     showSlide(currentSlide - 1);
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     showSlide(currentSlide);
-// });
-
+// create ajax error callback
+function ajaxError(request,error){
+	  console.log("Oops:", error);
+}
